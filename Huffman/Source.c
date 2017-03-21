ada8abcced23fa2f11531d9590a8088ffaedd5ce
S1x6 <v.korshunov@g.nsu.ru>
2017-03-21 13:10:20 +0700

    Update Source.c

--- Huffman/Source.c
+++ Huffman/Source.c
@@ -3,22 +3,28 @@
 #include "compressor.h"
 #include "decompressor.h"
 
+void copyFile(FILE * in, FILE * out);
 
 int main()
 {
-	FILE * fin = fopen("in.txt","rb");
+	FILE * fin = fopen("in.txt", "rb");
 	FILE * fout = fopen("out.txt", "wb");
-	if (!fin || !fout)
+	FILE * fcopy = fopen("in_copy.txt", "wb");
+	if (!fin || !fout){
 		printf("No such file in directory or couldn't create output file");
-
+		return 0;
+	}
+	
 	char type;
 	fscanf(fin, "%c\n", &type);
 	if (type)
 		switch (type) {
-		case 'd': 
+		case 'd':
 			decompress(fin, fout);
 			break;
 		case 'c':
+			//copyFile(fin, fcopy);
+			fseek(fin, 3, SEEK_SET);
 			compress(fin, fout);
 			break;
 		default:
@@ -26,4 +32,14 @@ int main()
 			break;
 	}
 	return 0;
+}
+
+void copyFile(FILE * in, FILE * out)
+{
+	char a, c = 0;
+	c = fread(&c, 1, 1, in);
+	while (c){
+		fwrite(&a, 1, 1, out);
+		c = fread(&a, 1, 1, in);
+	}
 }
